1b909704f05b91b1e6a4352b5a4f0586ada62fe7
svpustovoit <svpustvoit@gmail.com>
2026-05-11 15:52:27 +0300

    app goods_slug_converter_for_categories

--- store/goods/views.py
+++ store/goods/views.py
@@ -5,9 +5,12 @@ from goods.models import Products
 # Create your views here.
 
 
-def catalog(request):
+def catalog(request, category_slug):
 
-    goods = Products.objects.all()
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        goods = Products.objects.filter(category__slug=category_slug)
 
     context = {
         "title": "CLOTHING - Каталог",
